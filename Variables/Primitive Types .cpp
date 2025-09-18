03766a10243bbfe03f1b16af6e845fb71149968b
Erica Quintero <159857032+nenamuse@users.noreply.github.com>
2025-09-18 12:03:59 -0500

    Add example of primitive types in C++

---
+++ Variables/Primitive Types .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ // Creating a variable to store integer
+    int var = 10;
+  	cout << var;
+
+// Character variable
+  	char c = 'A';  
+	  cout << c;
+
+// Using hexadecimal base value
+  	x = 0x15;
+  	cout << x;
+
+// Creating a boolean variable
+	bool isTrue = true;  
+	cout << isTrue;
+
+// Floating point variable with a decimal value
+  	float f = 36.5;
+  	cout << f;
+
+// double precision floating point variable
+  	double pi = 3.1415926535;
+  	cout << pi;
+    
+    
+    return 0;
+}
